236f99bc9e71cc79e10eeb8ab9919f5ccb04d919
James Edwards <jamese@syapse.com>
2015-03-07 19:57:28 -0800

    change if def to js

--- js-builder.js
+++ js-builder.js
@@ -5,25 +5,20 @@ var Tree = require('./tree.js').Tree;
 var exec = require('child_process').exec;
 var stateFactory = require('./state.js');
 var parser = require('./parser_module.js');
-var functions = require('./functionsFactory.js')();
-var printTree = require('./printTree.js');
 
+var functions = {};
+functions.Tree = Tree;
 funcs = new Set(['+','-','*','/','>']);
 letters = new Set(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']);
 LETTERS = new Set(['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']);
-ARG_IDX = {
-	'X':0,
-	'Y':1,
-	'Z':2
-};
-DEFINED = {};
+
 CURRENT_TYPE = null;
 
 var variables = {
 	vars: {},
 	newVariable: function(){
 		var result = '';
-		for(var i = 0; i < 8; i++){
+		for(var i = 0; i < 16; i++){
 			result += letters.rnd();
 		}
 		if(this.vars[result] !== undefined){
@@ -41,7 +36,8 @@ module.exports = function(stack, name, funcdefs){
 	var command;
 	var result = '';
 	createFuncDefs(stack);
-	stack = identifyPrintFuncs(stack);
+	stack = replaceCustomFuncs(stack);
+	identifyPrintFuncs(stack);
 	while(stack.length){
 		command = stack.shift();
 		result = buildFunctions(command, result);
@@ -50,34 +46,18 @@ module.exports = function(stack, name, funcdefs){
 	result = '#include \"base.h\"\n\n'+PRE_MAIN+'int main(){'+ result + '};';
 	fs.writeFileSync('output.c', result);
 	exec('gcc output.c -o '+name+'.out', function(){
-		// exec('rm -rf output.c');
+		exec('rm -rf output.c');
 		return;
 	});
 };
 
 
-function buildFunctions(tree, result, argNames){
+function buildFunctions(tree, result){
 	if(tree.data.type === 'value' || tree.data.type === 'string'){
 		result += tree.data.value;
 	}
 	else if(tree.data.type === 'array'){
-		var arrayName = variables.newVariable();
-		var structName = variables.newVariable();
-		var newStruct = 'int '+arrayName+'['+tree.data.value.length+'] = {'+tree.data.value.toString()+'};';
-		newStruct += 'struct Array '+structName+';'+structName+'.body = '+arrayName+';';
-		newStruct += structName+'.len = '+tree.data.value.length+';';
-		result = newStruct + result;
-		result += structName;
-	}
-	else if(tree.data.type === 'custom'){
-		result += DEFINED[tree.data.value].name+'(';
-		for(var i = 0; i < tree.children.length; i++){
-			result = buildFunctions(tree.children[i], result);
-			if(i !== tree.children.length-1){
-				result += ',';
-			}
-		}
-		result += ')';
+		result += tree.data.value;
 	}
 	else if(tree.data.type === 'function' && tree.data.value !== '?'){
 		result += sys.map[tree.data.value][0];
@@ -92,92 +72,47 @@ function buildFunctions(tree, result, argNames){
 	else if(tree.data.value === '?'){
 		var arg_one = tree.children[1], arg_two = tree.children[2];
 		var condition = tree.children[0];
-		var condition_type = returnType(condition);
 		var arg_one_name = variables.newVariable(), arg_two_name = variables.newVariable();
 		var wrapper_name = variables.newVariable();
-		var arg_one_type = returnType(arg_one), arg_two_type = returnType(arg_two);
-		var argumentOneText = getArguments(arg_one, argNames);
-		var argumentTwoText = getArguments(arg_two, argNames);
-		var conditionText = getArguments(condition, argNames);
-		var head = ''+arg_one_type+' '+arg_one_name+'('+printArguments(argumentOneText, arg_one_type)+'){'+arg_one_type+' '+arg_one_name+' = '+buildFunctions(arg_one,'')+'; return '+arg_one_name+';};';
-		head += arg_two_type+' '+arg_two_name+'('+printArguments(argumentTwoText, arg_one_type)+'){'+arg_two_type+' '+arg_two_name+' = '+buildFunctions(arg_two,'')+'; return '+arg_two_name+';};';
-		head += arg_one_type+' '+wrapper_name+'('+printArguments(conditionText, arg_one_type)+'){ int truthval = istrue('+buildFunctions(condition,'')+');'+
-		arg_one_type+' result;'+'if(truthval){ result = '+arg_one_name+'('+printArguments(argumentOneText)+');}else{ result='+arg_two_name+'('+printArguments(argumentTwoText)+');} return result;};';
-		tail = ''+wrapper_name+'(';
-		if(argNames.length){
-			for(var i = 0; i < argNames.length; i++){
-				tail += argNames[i];
-				if(i !== argNames.length-1){
-					tail += ',';
-				}
-			}
-		}
-		tail += ')';
+		var head = 'var 'arg_one_name+'=(){'+arg_one_name+' = '+buildFunctions(arg_one,'')+'; return '+arg_one_name+';};';
+		head += 'var 'arg_two_name+'=(){'+arg_two_name+' = '+buildFunctions(arg_two,'')+'; return '+arg_two_name+';};';
+		head += wrapper_name+'(){ var truthval = istrue('+buildFunctions(condition,'')+');'+
+		'var result;'+'if(truthval){ result = '+arg_one_name+'();}else{ result='+arg_two_name+'();} return result;};';
+		tail = ''+wrapper_name+'()';
 		PRE_MAIN = PRE_MAIN + head;
 		result = result+tail;
 	}
 	return result;
 };
 
-function getArguments(tree, argNames){
-	if(argNames === undefined){
-		return [];
-	}
-	var results = [];
-	recurse(tree);
-	return results;
-
-	function recurse(tree){
-		if(argNames.indexOf(tree.data.value) !== -1){
-			results.push(tree.data.value);
-		}
-		if(tree.children.length){
-			for(var i = 0; i < tree.children.length; i++){
-				recurse(tree.children[i]);
-			}
-		}
-	};
-};
-
-function printArguments(arguments, type){
-	if(!arguments.length){
-		return '';
-	}
-	var result = '';
-	for(var i = 0; i < arguments.length; i++){
-		result += (type === undefined ? '' : type+' ') + arguments[i];
-		if(i !== arguments.length-1){
-			result += ',';
-		}
-	}
-	return result;
-}
-
 function createFuncDefs(stack){
 	while(stack.length && stack[0].data.type === 'funcdef'){
 		var tree = stack.shift();
 		if(tree.data.action === 'EACH'){
-			var iterator = tree.data.iterator;
-			body  = 'root = root.children[0]; var topLevel = this.buildFunc("'+iterator+'");';
-			body += 'var iteratorTree = topLevel;';
+			var funcData = tree.data.iterator.replace(/\(|\)/g,'').split(' ');
+			var funcName = funcData[0];
+			var funcElement = funcData[1];
+			body  = 'root = root.children[0];var mainTree = new this.Tree();';
+			body += 'var tree = mainTree;';
+			body += 'tree.set("type", "function"); tree.set("value","'+funcName+'");';
 			body += 'for(var i = 0; i < root.data.value.length; i++){';
-			body += 'var current = root.data.value[i];';
-			body += 'if(i === 0){ iteratorTree = this.replaceVars(iteratorTree, current); }';
-			body += 'iteratorTree = iteratorTree.insert(this.buildFunc("'+iterator+'"));'
-			body += 'iteratorTree = this.replaceVars(iteratorTree, current);'
-			body += 'if(i === root.data.value.length-1){ ';
-			body += 'iteratorTree = iteratorTree.insert(); iteratorTree.set("type","value");' 
-			body += 'iteratorTree.set("value",'+tree.data["null"]+');}}';
-			body += 'return topLevel;';
+			if(letters.contains(funcElement)){
+				body += 'var child = tree.insert(); child.set("type","value"); child.set("value",root.data.value[i]);';
+			} else {
+				body += 'var child = tree.insert(); child.set("type","value"); child.set("value",'+funcElement+');';
+			}
+			body += 'if(i === root.data.value.length-1){';
+			body += 'var child = tree.insert(); child.set("type","value"); child.set("value",'+tree.data["null"]+');}';
+			body += 'else{tree = tree.insert(); tree.set("type", "function"); tree.set("value","'+funcName+'");}}';
+			body += 'return mainTree;';
 			var func = new Function('root',body);
 			functions[tree.data.name] = func;
-		} else {
+		}
+		else {
 			var state = stateFactory();
 			state.body = tree.data.iterator;
-			var result = parser(state, [], DEFS)[0];
-			result = identifyPrintFuncs(result)[0];
-			var type = returnType(result);
-			writeCustomFuncs(result, type, tree);
+			var result = parser(state, [], DEFS);
+			functions[tree.data.name] = fillVariables(result[0]);
 		}
 	}
 	return;
@@ -215,65 +150,40 @@ var fillVariables = function(tree){
 	}
 };
 
-function writeCustomFuncs(tree, type, definition){
-	var args = definition.data.arguments;
-	var name = definition.data.name;
-	var funcBody = '', argNames = [];
-	var funcName = variables.newVariable();
-	funcBody += type+' '+funcName +'(';
-	for(var i = 0; i < args.length; i++){
-		argNames.push(variables.newVariable());
-		funcBody += type +' '+argNames[i];
-		if(i !== args.length-1){
-			funcBody += ', ';
-		} else { funcBody += '){';}
-	}
-	DEFINED[name] = {name: funcName, arguments: argNames};
-	tree = insertVariableNames(tree, argNames);
-	printTree(tree);
-	var result = variables.newVariable();
-	funcBody += type+' '+result+' = '+buildFunctions(tree, '', argNames)+';';
-	funcBody += 'return '+result+';};';
-	var placeholders = ['X','Y','Z'];
-	PRE_MAIN += funcBody;
-	return;
-};
-
-function insertVariableNames(tree, argNames){
-	tree = changeVar(tree);
-	return tree;
-
-	function changeVar(tree){
-		var placeholders = ['X','Y','Z'];
-		if(tree.data.type === 'value'){
-			for(var i = 0; i < placeholders.length; i++){
-				if(tree.data.value === placeholders[i]){
-					tree.data.value = argNames[i];
-				}
+var replaceCustomFuncs = function replaceCustomFuncs(stack){
+	var temp;
+	for(var i = 0; i < stack.length; i++){
+		if(stack[i].data.type === 'custom'){
+			stack[i] = functions[stack[i].data.value](stack[i]);
+		} 
+		if(stack[i].children.length){
+			for(var k = 0, l = stack[i].children.length; k < l; k++){
+				stack[i].children[k] = recurse(stack[i].children[k]);
 			}
 		}
+	}
+	return stack;
+
+	function recurse(tree){
+		if(tree.data.type === 'custom'){
+			tree = functions[tree.data.value](tree);
+		}
 		if(tree.children.length){
-			for(var k = 0; k < tree.children.length; k++){
-				changeVar(tree.children[k]);
+			for(var i = 0, l = tree.children.length; i < l; i++){
+				tree.children[i] = recurse(tree.children[i], root);
 			}
 		}
 		return tree;
-	}
+	};
 };
 
 function identifyPrintFuncs(stack){
-	if(!Array.isArray(stack)){
-		stack = [stack];
-	}
 	for(var i = 0; i < stack.length; i++){
 		if(stack[i].data.value === '@'){
 			changePrintFunc(stack[i]);
 		}
 		descend(stack[i]);
 	}
-
-	return stack;
-
 	function descend(tree){
 		for(var k = 0; k < tree.children.length; k++){
 			if(tree.children[k].data.value === '@'){
@@ -341,9 +251,6 @@ function returnType(tree) {
 		}
 		else if(type === 'boolean'){
 			return 'char';
-		}
-		else if(type === 'variable'){
-			return returnType(tree.children[1]);
 		}
 	}
 	else if(tree.data.type === 'value'){
